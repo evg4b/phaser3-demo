d8270b2a720a7ec42ec0d13614e7c72971568650
Evgeny Abramovich <evg.abramovitch@gmail.com>
2020-08-06 15:58:49 +0300

    Removed pwa and unused code. Added eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    Phaser: 'readonly',
+  },
+  settings: {
+    'import/extensions': ['.ts', '.js'],
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.ts',
+        ],
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'ignorePackages', {
+      ts: 'never',
+      js: 'never',
+    }],
+  },
+};
